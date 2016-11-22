4d99ad982574454f2ac8e2d257f451a851650ecf
David Davydov <kctrlv@gmail.com>
2016-11-21 20:27:49 -0700

    user signup and login

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :games, only: [:index]
+
+  root to: 'home#show'
+  resources :users, only: [:create]
 end
