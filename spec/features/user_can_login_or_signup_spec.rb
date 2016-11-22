947426fe326688e34e45582ec8969b767c5c5e4a
David Davydov <kctrlv@gmail.com>
2016-11-21 19:32:06 -0700

    spec user login and signup with sad paths

--- spec/features/user_can_login_or_signup_spec.rb
+++ spec/features/user_can_login_or_signup_spec.rb
@@ -9,4 +9,25 @@ RSpec.feature "User Authentication" do
     expect(current_path).to eq games_path
     expect(page).to have_content("Welcome, steve!")
   end
+
+  scenario "Visitor can login with created account" do
+    steve = User.create!(name: 'steve', password: '1234')
+    visit '/'
+    fill_in 'Name', with: 'steve'
+    fill_in 'Password', with: '1234'
+    click_button "Enter"
+    expect(current_path).to eq games_path
+    expect(page).to have_content("Welcome, steve!")
+  end
+
+  scenario "Visitor cannot create account that already exists" do
+    steve = User.create!(name: 'steve', password: '1234')
+    visit '/'
+    fill_in 'Name', with: 'steve'
+    fill_in 'Password', with: '2345'
+    click_button "Enter"
+    expect(current_path).to eq '/'
+    expect(page).to have_content("Incorrect password; user steve already exists")
+  end
+
 end
