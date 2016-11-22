2d67274ec46f2b4be47458dbd247b83b3bd3e504
David Davydov <kctrlv@gmail.com>
2016-11-21 20:49:21 -0700

    user logout and refactor specs

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  resources :games, only: [:index]
-
   root to: 'home#show'
+
+  resources :games, only: [:index]
   resources :users, only: [:create]
+
+  get '/logout', to: 'sessions#destroy'
+  
 end
